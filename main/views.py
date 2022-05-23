b91a542cebd861654dc24be7ed2fa55d55b7761a
kamilovkz <001kamilov@gmail.com>
2022-05-23 11:19:23 +0600

    Update multi searching

--- main/views.py
+++ main/views.py
@@ -33,30 +33,32 @@ def create(request):
 
 def api(request):
     keyApi = 'AXJCAMW4DHQ3K7V3W2TZ2ZMGJ6QSVDU2F6'
-    url = 'https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=' + keyApi 
+    url = 'https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=' + keyApi 
     
     ethAddresses = ethSearch.objects.all()
-    print(ethAddresses)
+    # print(ethAddresses)
 
     all_addresses = []
     for targetAddress in ethAddresses:
         res = requests.get(url.format(targetAddress)).json()
         jsonTbl = res["result"]
+        # print(jsonTbl)
         i = 0
         for result_item in jsonTbl:
             item_dict = jsonTbl[i]
-            print("From: " + item_dict["from"])
+            # print("From: " + item_dict["from"])
             i = i + 1
+            target_address = {
+                'hash': item_dict["hash"],
+                'sender': item_dict["from"],
+                'receiver': item_dict["to"],
+                'valueEth': item_dict["value"],
+                'confirmation': item_dict["confirmations"]
+            }
+            all_addresses.append(target_address)
             
+  
         
-        # target_address = {
-        #     'hash': res["result"][0]["hash"],
-        #     'sender': res["result"][0]["from"],
-        #     'receiver': res["result"][0]["to"],
-        #     'valueEth': res["result"][0]["value"],
-        #     'confirmation': res["result"][0]["confirmations"]
-        # }
-        # all_addresses.append(target_address)
 
     information = {'all_info': all_addresses}
 
